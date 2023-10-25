10bf03a508f9fef9fd938e48eaa1bc0d6182a81b
Joshua Howard <jhoward@cloudflare.com>
2023-10-25 10:08:17 -0400

    Test smart placement

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS urls;
-CREATE TABLE IF NOT EXISTS urls (short_path TEXT, original_url TEXT);
-INSERT INTO urls (short_path, original_url) VALUES ('x.com', 'example.com');
+DROP TABLE IF EXISTS Customers;
+CREATE TABLE IF NOT EXISTS Customers (CustomerId INTEGER PRIMARY KEY, CompanyName TEXT, ContactName TEXT);
+INSERT INTO Customers (CustomerID, CompanyName, ContactName) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,21 @@
 interface Env {
-	SHORT_URLS_DB: D1Database;
+	DB: D1Database;
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
-	  try {
-		const url = new URL(request.url);
-		if (url.pathname.startsWith('/testwrite')) {
-		  return await testWrite(env);
-		} else if (url.pathname.startsWith('/testread')) {
-		  return await testRead(env);
-		} else {
-		  return new Response('Choose an action: /testwrite or /testread');
-		}
-	  } catch (error: any) {
-		console.error(error); // Log the error for further debugging
-		return new Response(`Error: ${error.message}`, { status: 500 });
-	  }
-	}
-  };
-  
-  async function testWrite(env: Env) {
-	try {
-	  console.log("Testing simple insertion");  // Logging
-	  const stmt = env.SHORT_URLS_DB.prepare('INSERT INTO urls (short_path, original_url) VALUES (?, ?)').bind('testwrite', 'https://testwrite.com');
-	  const result: any = await stmt.run();
-	  console.log("Insertion result:", result);  // Logging
-  
-	  if (result && result.changes > 0) {
-		return new Response('Test insertion successful');
-	  } else {
-		console.error("Test insertion failed");  // Logging
-		return new Response('Test insertion failed', { status: 500 });
-	  }
-	} catch (error: any) {
-	  console.error("Test insertion error:", error);  // Logging
-	  return new Response(`Test Database Error: ${error.message}`, { status: 500 });
-	}
-  }
-  
-  async function testRead(env: Env) {
-	try {
-	  console.log("Testing simple read");  // Logging
-	  const stmt = env.SHORT_URLS_DB.prepare('SELECT original_url FROM urls WHERE short_path = ?').bind('x.com');
-	  const result: any = await stmt.run();
-	  console.log("Read result:", result);  // Logging
-  
-	  if (result && result.original_url) {
-		return new Response(`Test read successful: ${result.original_url}`);
-	  } else {
-		console.error("Test read failed");  // Logging
-		return new Response('Test read failed', { status: 500 });
-	  }
-	} catch (error: any) {
-	  console.error("Test read error:", error);  // Logging
-	  return new Response(`Test Database Error: ${error.message}`, { status: 500 });
-	}
-  }
+		console.log(`request.cf?.colo: ${JSON.stringify(request.cf?.colo)}`);
+	
+		const statement = env.DB.prepare("SELECT ContactName FROM Customers WHERE CompanyName = ?");
+
+		const start = Date.now();
+		let { results: partialResults1 } = await statement.bind("Alfreds Futterkiste").all();
+		let { results: partialResults2 } = await statement.bind("Around the Horn").all();
+		let { results: partialResults3 } = await statement.bind("Bs Beverages").all();
+		const latency = Date.now() - start;
+		const results = [...partialResults1, ...partialResults2, ...partialResults3];
+
+		console.log(`latency: ${latency}`);
+		return new Response(JSON.stringify(results));
+	},
+};
